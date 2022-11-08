139215ab2b381722bc75246ccd0d9b7c4305f0cb
Remix Workspace <>
2022-11-07 20:55:14 -0600

    receive and fallback functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -59,4 +59,14 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "call failed");
     }
+
+    //receive
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+    //fallback <- when we send data
 }
